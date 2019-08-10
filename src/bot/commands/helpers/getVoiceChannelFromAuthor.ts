e4b9830435ac246875e932305567cba7c8bcebf4
Nick Schiffer <nick@schiffer.us>
2019-08-10 01:33:27 -0700

    Send "Please join a channel" to author

--- src/bot/commands/helpers/getVoiceChannelFromAuthor.ts
+++ src/bot/commands/helpers/getVoiceChannelFromAuthor.ts
@@ -5,7 +5,7 @@ import localize from '@util/i18n/localize';
 const getVoiceChannelFromAuthor = (message: Message) => {
   const { voiceChannel } = message.member;
   if (!voiceChannel) {
-    message.reply(localize.t('helpers.voiceChannelFinder.error'));
+    message.author.send(localize.t('helpers.voiceChannelFinder.error'));
   }
 
   return voiceChannel;
